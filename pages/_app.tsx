5049b1fc637e31529f6111fa3d65854d40a5459e
kristene1433 <kristenepom@gmail.com>
2025-03-15 11:06:52 -0700

    fix dashboard navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
-// pages/_app.tsx
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css';
-import Footer from '../components/Footer';
+import { useRouter } from 'next/router';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // Determine whether we’re on the dashboard route
+  const isDashboard = router.pathname.startsWith('/dashboard');
+
   return (
-    <SessionProvider session={session}>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <NavBar />
-        <main className="flex-1">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-    </SessionProvider>
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      {/* Conditionally render NavBar unless we're on /dashboard */}
+      {!isDashboard && <NavBar />}
+
+      <main className="flex-1 container mx-auto px-4 py-12">
+        <Component {...pageProps} />
+      </main>
+
+      {/* Also conditionally render Footer if you want to hide that too */}
+      {!isDashboard && <Footer />}
+    </div>
   );
 }
 
